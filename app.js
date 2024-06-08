d431d643b24adfd37b2f2615ed048797f2995f9f
Omer Sevik <omer.sevik@hotmail.fr>
2024-06-08 17:39:36 +0200

    create user and signup

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 
 mongoose.connect('mongodb+srv://omersevik:Franceturquie-1@backend.ksoh2eg.mongodb.net/?retryWrites=true&w=majority&appName=backend',
     {
@@ -23,5 +24,6 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
